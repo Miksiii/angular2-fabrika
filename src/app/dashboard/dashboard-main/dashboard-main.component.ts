92bf9973a709d33c83d2d667c66fb2146d4e2519
milanhakajprodakshn@gmail.com <milanhakajprodakshn@gmail.com>
2017-03-19 17:43:41 +0100

    login, signup, dashboard-main added

---
+++ src/app/dashboard/dashboard-main/dashboard-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-main',
+  templateUrl: './dashboard-main.component.html',
+  styleUrls: ['./dashboard-main.component.less']
+})
+export class DashboardMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
